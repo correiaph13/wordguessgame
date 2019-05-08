a3796e5c2e90a8ba20ecb5aea64449f27b9da68d
Hibi <correiaph13@gmail.com>
2019-05-07 20:12:08 -0500

    initial folders and pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//game begins when user presses any key (create a scrreen for this)
+
+// JS picks a random item from the word list array; something about math function random
+
+
+// display the empty spaces to user
+
+
+//user uses keyboard to guess letters (event)
+
+// once user pics a letter, check if it is in the string; if its not then mark as a miss, if it is as a hit
+
+// missed letters will be added to the new array of wrong leters and displayed on the side somewhere
+
+//missed letters will also add to the number of misses (how many allowed?) and add to the graph counter?
+
+
+
+// hits will display the letter in the word 
+
