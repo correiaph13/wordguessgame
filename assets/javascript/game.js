ba3a4fa8f23c202eff2347d1175b4329c4ae7d15
Hibi <correiaph13@gmail.com>
2019-05-10 13:51:01 -0500

    wordbank and initial variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,29 @@
-//game begins when user presses any key (create a screen for this)
+window.onload = function () {
 
 
-// JS picks a random item from the word list array; something about math function random
-   // JS picks a random item from the word list array; something about math function random
-   //find out if i can reference a list from another file
-const elements = ['Hydrogen', 'oxygen', 'fluorine', 'argon','mercury'];
-
-console.log(elements.indexOf('oxygen'));
-
-var randomWord = elements[Math.floor(Math.random() * elements.length)];
-
-console.log(randomWord);
-
-document.write(randomWord);
-
-
-// display the empty spaces to user
-
-
-//user uses keyboard to guess letters (event)
-
-// once user pics a letter, check if it is in the string; if its not then mark as a miss, if it is as a hit
-
-// missed letters will be added to the new array of wrong leters and displayed on the side somewhere
-
-//missed letters will also add to the number of misses (how many allowed?) and add to the graph counter?
-
-
-
-// hits will display the letter in the word 
-
+    var wordToGuess //wordToGuess
+    
+    var wordToGuessLength //length of word to wordToGuess
+    
+    var lettersInWordToGuess //letters in wordToGuess
+    
+    var lettersGuessed //letters guessed
+    
+    var lettersCorrect // correct and incorrect letters
+    
+    var letttersIncorrect
+    
+    var maxGuessNumber = 6 //number of guesses, lets say 6
+    
+    var guessesLeft 
+    
+    
+    
+    //obtain random word from wordbank in separate file
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+    
+    console.log(randomWord);
+    console.log(words.length);
+    
+    
+    };
