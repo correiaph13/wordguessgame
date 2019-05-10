d4e6875a9857762d0192451015a51255a7e9bb63
Hibi <correiaph13@gmail.com>
2019-05-10 15:01:54 -0500

    wired html to js file for displaying wins/loses and current word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,74 @@
 window.onload = function () {
 
-
-    var wordToGuess //wordToGuess
+    // var wordToGuess //wordToGuess
     
-    var wordToGuessLength //length of word to wordToGuess
+    var lettersGuessed = event.key;//letters guessed
     
-    var lettersInWordToGuess //letters in wordToGuess
-    
-    var lettersGuessed //letters guessed
-    
-    var lettersCorrect // correct and incorrect letters
-    
-    var letttersIncorrect
+    var letttersIncorrect = ["h","j"];
     
     var maxGuessNumber = 6 //number of guesses, lets say 6
     
-    var guessesLeft 
-    
-    
+    var guessesLeft = 1
     
     //obtain random word from wordbank in separate file
-    var randomWord = words[Math.floor(Math.random() * words.length)];
-    
-    console.log(randomWord);
-    console.log(words.length);
+    var wordToGuess = words[Math.floor(Math.random() * words.length)];
     
     
+    let wins = 4;
+    let losses = 5;
+    
+          
+      // // Create variables that hold references to the places in the HTML where we want to display things.
+      //   var lettersGuessedText = document.getElementById("lettersGuessed");
+      //   var lettersCorrectText = document.getElementById("lettersCorrect");
+      //   var lettersIncorrectText = document.getElementById("letttersIncorrect");
+    var winsText = document.getElementById("winsText");
+    var lossesText = document.getElementById("lossesText");
+    var wordToGuessText = document.getElementById("wordToGuess");
+    var guessesLeftText = document.getElementById("guessesLeftText");
+    var lettersIncorrectText = document.getElementById("lettersIncorrectText");
+    
+    
+    // Display the user and computer guesses, and wins/losses/ties
+          winsText.textContent = "wins: " + wins;
+          lossesText.textContent = "losses: " + losses;
+          wordToGuessText.textContent = "your word is: " + wordToGuess.word;
+          guessesLeftText.textContent = "remaining guesses: " + guessesLeft;
+          lettersIncorrectText.textContent = "incorrect letters: "+ letttersIncorrect;
+          
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+    // Determines which key was pressed.
+    var userGuess = event.key;
+          
+    console.log(userGuess)};
+           
     };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
